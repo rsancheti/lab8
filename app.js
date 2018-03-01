5a60fd83d7420b1d980cba91b9c93a361adfa7de
Rajiv Sancheti <rsanchet@ucsd.edu>
2018-03-01 10:20:44 -0800

    thirdpartyAPI lab

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/rsvp', rsvp.adminView);
+app.get('/', index.view);
+app.get('/rsvp', rsvp.adminView);
+app.post('/addRSVP', rsvp.addRSVP);
 
 // Example route
 // app.get('/users', user.list);
